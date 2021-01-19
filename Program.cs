e6391757038d0c050d86b1f6a948cbf870e5500f
Lijing Wang <lijing_wang@qbsol.com>
2021-01-19 09:55:14 -0700

    added razor page for view, added npm packages

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTree
+{
+    public class Program
+    {
+        // A console app that listens to the web request
+        // Can be started with IIS Express OR call the executable
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
