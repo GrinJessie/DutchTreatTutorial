22e6d6f68e4fa1af932ce02c3f621757cf0593ab
Lijing Wang <lijing_wang@qbsol.com>
2021-01-19 11:20:16 -0700

    replaced the static file with MVC, set up the basic routing and added related service

--- Startup.cs
+++ Startup.cs
@@ -1,29 +1,51 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace DutchTree
+namespace DutchTreat
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // Set up the dependency services that can be found in the system
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            // Exception page can be shown to developers
+            app.UseDeveloperExceptionPage();
+
+            // The order of the middleware matters
+            // Middleware = pipelines
+
+            // Points the default folder to wwwroot
+            // app.UseDefaultFiles();
+
             app.UseStaticFiles();
+
+            // Front-end package management needs a nuget package
             app.UseNodeModules();
+
+            // Generic routing inside of ASP dotnet core
+            app.UseRouting();
+            
+            // For MVC routing decisions
+            app.UseEndpoints(configure =>
+            {
+                // How to find a specific controller using these semantics 
+                // Name the route
+                // Look for controller name that matches the names inside our controllers
+                // Look for action call in the controller with matched names
+                configure.MapControllerRoute("Fallback", "{controller}/{action}/{id?}",
+                    // If none of them matched, use the default below
+                    new { controller = "App", action ="Index" });
+            });
+
         }
     }
 }
